fff26e81172031fef8bc72ddd4d5564258aa536d
root <root@ip-10-0-9-199.ap-south-1.compute.internal>
2025-05-05 13:19:35 +0000

    done

--- infra/backend.tf
+++ infra/backend.tf
@@ -3,13 +3,3 @@ terraform {
     path = "./terraform.tfstate"
   }
 }
-
-#terraform {
-#  backend "s3" {
-#    bucket         = "naveen-1812001-terraform-state-bucket"
-#    key            = "global/s3/terraform.tfstate"
-#    region         = "ap-south-1"
-#    encrypt        = true
-#    use_lockfile   = true
-#  }
-#}
